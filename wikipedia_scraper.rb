0bd64e245a1571733a92b5d9ec433fa82663a5cb
Roy Wei <roywjy@gmail.com>
2016-06-18 18:08:39 +0800

    finish NBA wiki page scrapper

--- wikipedia_scraper.rb
+++ wikipedia_scraper.rb
@@ -8,4 +8,8 @@ require 'csv'
 url = "https://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
 page = Nokogiri::HTML(open(url))
 
-puts page
+# puts page.css('td[style="text-align:left;"]').text
+
+page.css('td[style="text-align:left;"]').each do |line|
+  puts line.text
+end
