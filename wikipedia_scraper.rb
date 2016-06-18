cb9390138ec5b0611317c816b4fbcad4a772f4cc
Roy Wei <roywjy@gmail.com>
2016-06-18 17:59:01 +0800

    fixed SSL Certificate problem

--- wikipedia_scraper.rb
+++ wikipedia_scraper.rb
@@ -1,11 +1,11 @@
+require 'open-uri'
 require 'HTTParty'
 require 'nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
-require 'curb'
 
 url = "https://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
-page = Curl.get(url)
+page = Nokogiri::HTML(open(url))
 
-puts page.body_str.css('a')
+puts page
