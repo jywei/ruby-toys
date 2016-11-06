4744fcea319b2bca6b23b48a34fcfb26d4d626a4
Roy Wei <roywjy@gmail.com>
2016-11-06 19:27:35 +0800

    update runner file

--- finance_project/historical_stock_quote.rb
+++ finance_project/historical_stock_quote.rb
@@ -1,13 +1,23 @@
 require 'yahoo-finance'
 require 'awesome_print'
 
-def historical_price(symbol)
-  yahoo_client = YahooFinance::Client.new
-  data = yahoo_client.historical_quotes(symbol, { start_date: Time::now - (24 * 60 * 60 * 365), end_date: Time::now })
-  data.map do |quote|
-    [quote.trade_date, quote.open]
+class HistoricalStockQuote
+  def self.historical_price(symbol)
+    yahoo_client = YahooFinance::Client.new
+    data = yahoo_client.historical_quotes(symbol, { start_date: Time::now - (24 * 60 * 60 * 365), end_date: Time::now })
+    data.map do |quote|
+      [quote.trade_date, quote.open]
+    end
+  end
+
+  def self.write_price_by_symbol(symbol)
+    quotes = historical_price(symbol)
+    filename = "#{symbol}.csv"
+    CSV.open(filename, "w") do |file|
+      quotes.each do |quote|
+        file << quote
+      end
+    end
   end
 end
-
-ap historical_price('GOOG')
 
--- finance_project/runner.rb
+++ finance_project/runner.rb
@@ -1,11 +1,16 @@
 require_relative 'stock_quote'
+require_relative 'historical_stock_quote'
 
-def stock_price_selector(*symbol_array)
-  symbol_array.each do |symbol|
-    stock_price = StockQuote.price_by_symbol(symbol)
-    stock_written = StockQuote.write_price_by_symbol(symbol, stock_price)
-  end
-end
+# def stock_price_selector(*symbol_array)
+#   symbol_array.each do |symbol|
+#     stock_price = StockQuote.price_by_symbol(symbol)
+#     stock_written = StockQuote.write_price_by_symbol(symbol, stock_price)
+#   end
+# end
 
-stock_price_selector('AAPL', 'GOOG', 'MSFT')
+# stock_price_selector('AAPL', 'GOOG', 'MSFT')
+
+
+puts HistoricalStockQuote.historical_price("MSFT")
+HistoricalStockQuote.write_price_by_symbol("MSFT")
 
