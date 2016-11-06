62e10604bb673b230ef086ed813c3b523d68f7c0
Roy Wei <roywjy@gmail.com>
2016-11-06 18:13:23 +0800

    start a finance project

---
+++ finance_project/stock_quote.rb
@@ -0,0 +1,20 @@
+require 'yahoo-finance'
+
+class StockQuote
+  def self.price_by_symbol(symbol)
+    client = YahooFinance::Client.new
+    data = client.quotes([symbol], [:open])
+    data[0].open
+  end
+
+  def self.write_price_by_symbol(symbol, price)
+    filename = "#{symbol}.csv"
+    todays_date = Time.now.strftime('%Y-%m-%d')
+    File.open(filename, "a") do |file|
+      file << "#{todays_date}, #{price}\n"
+    end
+  end
+end
+
+stock_price = StockQuote.price_by_symbol('AAPL')
+stock_written = StockQuote.write_price_by_symbol('AAPL', stock_price)
