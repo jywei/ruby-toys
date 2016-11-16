cce70fc4df30df8a23b17a8044ca948b4b03150d
Roy Wei <roywjy@gmail.com>
2016-11-16 21:53:11 +0800

    add timing

--- Ruby Rack/hello.rb
+++ Ruby Rack/hello.rb
@@ -5,4 +5,4 @@ app = proc do |env|
   ['200', {'Content-Type' => 'text/html'}, ['Hello, Rack!']]
 end
 
-Rack::Handler::WEBrick.run(app, Port: 8090, Host: '0.0.0.0')
+Rack::Handler::WEBrick.run(Timing.new(app), :Port => 8090, :Host => '0.0.0.0')
