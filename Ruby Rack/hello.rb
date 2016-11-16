d60b87edc1309647a9e09ba53f4198a68e4475de
Roy Wei <roywjy@gmail.com>
2016-11-16 21:55:30 +0800

    rakeup

--- Ruby Rack/hello.rb
+++ Ruby Rack/hello.rb
@@ -1,8 +1,14 @@
-require 'rack'
-require './timing.rb'
+# require 'rack'
+# require './timing.rb'
 
-app = proc do |env|
-  ['200', {'Content-Type' => 'text/html'}, ['Hello, Rack!']]
+# app = proc do |env|
+#   ['200', {'Content-Type' => 'text/html'}, ['Hello, Rack!']]
+# end
+
+# Rack::Handler::WEBrick.run(Timing.new(app), :Port => 8090, :Host => '0.0.0.0')
+
+class Hello
+  def self.call(env)
+    ['200', {'Content-Type' => 'text/html'}, ['Hello, Rack!']]
+  end
 end
-
-Rack::Handler::WEBrick.run(Timing.new(app), :Port => 8090, :Host => '0.0.0.0')
