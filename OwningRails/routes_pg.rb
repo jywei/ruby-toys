d816d239adb2845ca2d195aca198359d3457c6ff
Roy Wei <roywjy@gmail.com>
2016-11-20 19:56:33 +0800

    between merge & update and instance_eval

--- OwningRails/routes_pg.rb
+++ OwningRails/routes_pg.rb
@@ -1,19 +1,23 @@
 class Router
-  def initializer
+  def initialize
     @routes = {}
   end
 
-  def match
-    @routes.update route
+  def match(route)
+    # @routes.update route
+    @routes.merge! route
   end
 
-  def routes
-    yield
-    p routes
+  def routes(&the_proc)
+    # yield
+    instance_eval(&the_proc)  #proc to block
+    p @routes
   end
 end
 
-Router.new.routes do
+Router.new.instance_eval { p @routes }
+
+Router.new.routes do          #simple DSL
   match "/users" => "users#index"
   match "/login" => "sessions#new"
 end
