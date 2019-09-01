6f7056dba7007d89b962b8a5e739e1ac96bab031
Roy Wei <roywjy@gmail.com>
2019-09-01 14:24:22 +1000

    Add r2 ruby solution

---
+++ coding_exercises/r2.rb
@@ -0,0 +1,22 @@
+def rotate(given_array, _n)
+  given_array.transpose.each(&:reverse!)
+end
+
+# NOTE: The following input values will be used for testing your solution.
+a1 = [[1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]]
+# rotate(a1, 3) should return:
+# [[7, 4, 1],
+#  [8, 5, 2],
+#  [9, 6, 3]]
+
+a2 = [[1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]]
+# rotate(a2, 4) should return:
+# [[13, 9, 5, 1],
+#  [14, 10, 6, 2],
+#  [15, 11, 7, 3],
+#  [16, 12, 8, 4]]
